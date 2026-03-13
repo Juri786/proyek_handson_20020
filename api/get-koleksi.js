393f23da56e5ab4eb813cea8430103da8912646d
Juri786 <bagasvieri@gmail.com>
2026-03-13 15:30:06 +0700

    Fixing

--- api/get-koleksi.js
+++ api/get-koleksi.js
@@ -5,7 +5,7 @@ const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANO
 export default async function handler(req, res) {
   const { data, error } = await supabase
     .from('koleksi')
-    .select('judul, path');
+    .select('judul, pencipta, tahun, harga,  path');
 
   if (error) return res.status(500).json({ error: error.message });
   return res.status(200).json(data);
